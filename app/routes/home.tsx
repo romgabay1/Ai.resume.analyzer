901eac4f68cbdacfb7d2a31532ed24f6d9350d9c
Rommm <romgabay1@gmail.com>
2025-10-10 12:04:39 +0300

    Resume Feedback Page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -20,7 +20,7 @@ export default function Home() {
 
   useEffect(() => {
     if(!auth.isAuthenticated) navigate('/auth?next=/')
-  }, [auth.isAuthenticated, navigate])
+  }, [auth.isAuthenticated])
 
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover"> 
     <Navbar />
